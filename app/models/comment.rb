8c1522ed859684dd660bddbc147aa8827fa35007
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 02:00:50 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: :reject_user
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
 
   def user_attributes=(user_attributes)

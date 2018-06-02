89a228df1ecc10d3ef6729350aa1645530ee467c
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 01:06:53 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
   def unique_users
     self.users.uniq{|u| u}
   end

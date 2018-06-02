32e6433013e19f63ebd006f297586c55e6ad32e9
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 01:44:55 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
   def user_username=(username)
     self.user = User.find_or_create_by(username: username)

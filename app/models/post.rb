14c790c927b27f6279222bc7922e14723567e78f
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 01:06:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories
+
   has_many :comments
   has_many :users, through: :comments
 
+  def unique_users
+    self.users.uniq{|u| u}
+  end
 
 end
+
+
+# https://stackoverflow.com/questions/4252164/how-do-i-get-unique-elements-in-this-array

c7b436ffbe29c64d4a5672b224dab60da320955a
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 01:53:31 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
     puts "Params = #{params}"
+    puts "Comment Parms = #{comment_params}"
     redirect_to comment.post
   end
 

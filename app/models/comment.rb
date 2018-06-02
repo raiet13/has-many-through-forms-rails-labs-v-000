e82a8b1f7d5a7481354ba3d086ca3fcd51caa24b
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 02:02:37 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
     puts "User Attributes = #{user_attributes}"
-    if user_attributes.value is not nil
+    if user_attributes.value != ""
       self.user = User.find_or_create_by(user_attributes)
     end
   end
